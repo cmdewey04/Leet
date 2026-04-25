1cd8c0376358bdad89a103bbbbee532d45eedf7c
cmdewey04 <137228762+cmdewey04@users.noreply.github.com>
2026-04-25 14:57:50 -0500

    Create 189-RotateArray

---
+++ 189-RotateArray/version/python/189-RotateArray_20260425_145750.py
@@ -0,0 +1,10 @@
+# Last updated: 4/25/2026, 2:57:50 PM
+1class Solution:
+2    def rotate(self, nums: List[int], k: int) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5        """
+6        #
+7        k = k % len(nums)
+8        nums[k:]
+9        nums[:] = nums[-k:] + nums[:-k]
